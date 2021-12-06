f019ef9e981cf64c30934478a09abeba0eedee72
belhub <82940610+belhub@users.noreply.github.com>
2021-12-06 11:27:19 +0100

    Add files via upload

--- src/pl/lab/Main.java
+++ src/pl/lab/Main.java
@@ -1,20 +1,17 @@
 package pl.lab;
+
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.HashMap;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Random;
 
-public class Main extends JFrame{
+public class Main extends JFrame {
 
-    private static JLabel userLabel;
-    private static JTextField usernameText;
-    private static JLabel passwordLabel;
-    private static JPasswordField passwordText;
-    private static JButton loginButton;
-    private static JButton cancelButton;
-    private static JLabel success;
-    private static JPanel panel;
+    private JButton button = null;
+    private JButton cancel = null;
+    private Random r = new Random();
 
     public static void main(String[] args) {
         System.out.println("Program konsolowy");
@@ -34,75 +31,40 @@ public class Main extends JFrame{
     }
     public Main(String title) throws HeadlessException {
         super(title);
-        buildFrame();
+        RunningButton();
     }
-    protected void buildFrame(){
-        panel = new JPanel();
-        JFrame frame = new JFrame();
 
-        frame.setSize(450,340);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    public void  RunningButton(){
+        JPanel panel = new JPanel();
 
-        frame.add(panel);
+        setSize(500,500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        add(panel);
 
+        panel.setBorder(new EmptyBorder(5,5,5,5));
+        setContentPane(panel);
         panel.setLayout(null);
+        setTitle("Uciekający przycisk");
 
-        userLabel = new JLabel("Username");
-        userLabel.setBounds(40,20,80,20);
-        panel.add(userLabel);
-
-        usernameText = new JTextField();
-        usernameText.setBounds(120,20,150,20);
-        panel.add(usernameText);
-
-        passwordLabel = new JLabel("Password");
-        passwordLabel.setBounds(40,50,80,20);
-        panel.add(passwordLabel);
-
-        passwordText = new JPasswordField();
-        passwordText.setBounds(120,50,150,20);
-        panel.add(passwordText);
-
-        loginButton = new JButton("Login");
-        loginButton.setBounds(60,90,90,30);
-        loginButton.addActionListener(e -> actionPerformed());
-        panel.add(loginButton);
-
-        cancelButton = new JButton("Cancel");
-        cancelButton.setBounds(180,90,90,30);
-        cancelButton.addActionListener(e -> cancelButtonPress());
-        panel.add(cancelButton);
-
-        success = new JLabel("");
-        success.setBounds(20,150,50,50);
-        panel.add(success);
-
-        frame.setVisible(true);
-    }
-
-    private static void actionPerformed() {
-        Login loginData = new Login("Marek", "haslo1");
-        Login loginData2 = new Login("Jarek", "haslo2");
-        HashMap<String, Login> login = new HashMap<>();
-        login.put("Marek",loginData);
-        login.put("Jarek",loginData2);
-
-        String user=usernameText.getText();
-        String  pass=String.valueOf(passwordText.getPassword());
-
-        if (login.containsKey(user)){
-            if (login.get(user).getHaslo().equals(pass)){
-                panel.setBackground(Color.green);
+        button = new JButton("Button");
+        button.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                super.mouseEntered(e);
+                if(e.getX() <= button.getX() + button.getWidth()-10 && (getMousePosition().y >= button.getY() ||
+                        getMousePosition().y <= button.getY() + button.getHeight())) {
+                    button.setLocation(r.nextInt(panel.getWidth()-button.getWidth()), r.nextInt(panel.getHeight()-button.getHeight()));
+                }
             }
-            else
-                panel.setBackground(Color.red);
-        }
-        else
-            panel.setBackground(Color.red);
-    }
+        });
 
-    private static void cancelButtonPress() {
-        System.exit(0);
-    }
+        panel.add(button);
+        button.setBounds(100,100,100,40);
 
+        cancel = new JButton("Cancel");
+        cancel.setBounds(300,100,100,40);
+        cancel.addActionListener(e->button.setBounds(100,100,100,40));
+        add(cancel);
+
+    }
 }
